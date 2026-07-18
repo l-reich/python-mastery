693b3b59cb5151364795bb3047ebbefc8ba3a1d7
l-reich <64188912+l-reich@users.noreply.github.com>
2026-07-18 17:53:27 +0200

    ex3.1

--- Work/stock.py
+++ Work/stock.py
@@ -1,3 +1,5 @@
+import csv
+
 class Stock:
     def __init__(self, name, shares, price):
         self.name = name
@@ -6,3 +8,20 @@ class Stock:
 
     def cost(self):
         return self.shares * self.price
+
+    def sell(self, amt):
+        self.shares -= amt
+
+def read_portfolio(fn: str) -> list[Stock]:
+    res_list = []
+    with open(fn, 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            res_list.append(Stock(str(row[0]), int(row[1]), float(row[2])))
+
+    return res_list
+
+def print_portfolio(portffolio: list[Stock]) -> None:
+    for s in portfolio:
+           print('%10s %10d %10.2f' % (s.name, s.shares, s.price))
