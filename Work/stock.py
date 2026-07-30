69319df75aef5cc19a4d9f4cc68f52ebf34026d1
l-reich <64188912+l-reich@users.noreply.github.com>
2026-07-30 15:03:35 +0200

    ex5.4

--- Work/stock.py
+++ Work/stock.py
@@ -1,37 +1,17 @@
-from validate import PositiveInteger, PositiveFloat, String
+from typedproperty import String, Float, Integer
 
 
 class Stock:
-    name = String()
-    shares = PositiveInteger()
-    price = PositiveFloat()
-    # _types = [String, PositiveInteger, PositiveFloat]
+    name = String("name")
+    shares = Integer("shares")
+    price = Float("price")
+
     _types = [str, int, float]
 
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, val):
-        self._shares = PositiveInteger.check(val)
-
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, val):
-        self._price = PositiveFloat.check(val)
-
-    @property
-    def cost(self):
-        return self.shares * self.price
 
     def sell(self, amt):
         self.shares -= amt
